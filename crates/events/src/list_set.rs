e032a372477f3772f0862c8dd14f079f12daea33
triesap <tyson@radroots.org>
2026-06-27 08:31:06 +0000

    events: export dto events lists

--- crates/events/src/list_set.rs
+++ crates/events/src/list_set.rs
@@ -4,6 +4,7 @@ use crate::list::RadrootsListEntry;
 use alloc::{string::String, vec::Vec};
 
 #[cfg_attr(feature = "dto-bindgen", derive(dto_bindgen::Dto))]
+#[cfg_attr(feature = "dto-bindgen", dto(export))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Clone, Debug)]
 pub struct RadrootsListSet {
